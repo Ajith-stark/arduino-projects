16fefa7bbe27d5311eac6fa2e717d3dcfa9a6ffc
Ajith-stark <ajith.kum.12342@gmail.com>
2020-10-01 21:46:47 +0530

    modified blue

--- bluetooth_test/bluetooth_test.ino
+++ bluetooth_test/bluetooth_test.ino
@@ -1,18 +1,25 @@
-char BTdata;
+//char BTdata;
 
 int LEDs[4] ={3,4,5,6}; // pin 3 4 5 6 are LED1 LED2 .... 
 
 void setup(){
-    Serial.begin(9600);
-    for(int i=0;i<4; i++) pinMode(LEDs[i],OUTPUT); // make all pins output
+    	Serial.begin(9600);
+    	pinMode(LEDs[0],OUTPUT); // make all pins output
+	pinMode(LEDs[1],OUTPUT);
+	pinMode(LEDs[2],OUTPUT);  
+	pinMode(LEDs[3],OUTPUT); 
+
 }
 
 
 void loop(){
     if(Serial.available()>0){
      // if bluetooth provides data 
-        BTdata=Serial.read();
-        // Serial.print(BTdata); 
+
+       char BTdata=Serial.read();
+        Serial.print(BTdata);
+	Serial.print("\n");
+ 	// if (BTdata=='F') Serial.print("YEAAA works!\n");
         switch (BTdata){
             
             case 'F':
@@ -35,7 +42,7 @@ void loop(){
                 digitalWrite(LEDs[3],HIGH);
                 break;
         }
-        delay(1000);
+        //delay(1000);
 
     }else {
         // if bluetooth wont get any data turn off all
